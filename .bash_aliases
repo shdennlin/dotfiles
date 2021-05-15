02cb617b1f52932e55603d8f361a15bbf68835fa
shdennlin <shawndennislin@gmail.com>
2021-05-15 18:33:36 +0800

    reconfg install.sh

--- .bash_aliases
+++ .bash_aliases
@@ -1,84 +1,21 @@
-command_exists () {
-    type "$1" &> /dev/null ;
-}
+alias src='source ~/.bashrc'
 
-# gdrive download
-gdd () {
-  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
-  rm -rf /tmp/cookies.txt
-}
-# ============alias=============
-
-if command_exists bpytop ; then
-  alias top='bpytop'
-fi
-
-alias ca='conda activate'
-alias catg='conda activate tf-gpu'
-
-alias cl='clear'
 alias ..='cd ..'
-alias .2='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-alias .5='cd ../../../../..'
-alias .6='cd ../../../../../..'
-alias .7='cd ../../../../../../..'
-alias .8='cd ../../../../../../../..'
-alias .9='cd ../../../../../../../../..'
-
-if command_exists xclip ; then
-  alias c="xclip -selection clipboard"
-  alias cn="tr -d '\n' | xclip -selection clipboard"
-  alias pcn="pwd | tr -d '\n' | xclip -selection clipboard"
-fi
-
-alias dus="du -chd 1 | sort -h"
-
-alias dk="docker"
-
-# slove emacs Chinese display problem
-alias emacs='env LC_CTYPE=zh_TW.UTF-8 emacs'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
 
 alias g='git'
-alias gp='grep -Ei --color=always'
-alias gd='gdrive'
 
-alias l='ls --color=always'
-alias ll='ls -lhF --color=always'
-alias la='ls -AF --color=always'
-alias lal='ls -lAhF --color=always'
-alias lls='ls -lAhF --color=always | less -r'
-
-alias lt='ls -t --color=always'
-alias llt='ls -lhFt --color=always'
-alias lat='ls -AFt --color=always'
-alias lalt='ls -lAhFt --color=always'
-alias llst='ls -lAhFt --color=always | less -r'
-
-alias less='less -r'
-
-alias lsofp='sudo lsof -i -P -n | grep LISTEN'
-
-alias psgp='ps -ef | gp -E'
-
-alias su='sudo'
-alias suvi='sudo -E vi'
-alias srcb='source ~/.bashrc'
-
-
-alias k='kill'
-alias k9='kill -9'
-alias pk='pkill'
-alias pk9='pkill -9'
-
-alias rm='rm -rf'
-alias rmv='rm -rfv'
-
-alias vi='vim'
-alias vib='vi ~/.dotfiles/.bash_aliases'
-alias vig='vi ~/.dotfiles/.gitconfig'
+alias l='ls -alhH --color=always'
+alias la='ls -a'
+alias ll='ls -lahH --color=always'
+alias grep='\grep --color'
 
 # show git branch in terminal
 export PS1='\[\033[01;32m\]\[\033[0m\033[0;32m\]\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1)$ '
+
+if [ -f ~/.aliases ]; then
+    . ~/.aliases
+fi
--- install.sh
+++ install.sh
@@ -1,77 +1,128 @@
 #!/bin/sh
 
-install_A_check='_'
-echo -n "Do you want to install all(y/n): "
-read install_A_check
-while [[ $install_A_check != @('y'|'n') ]]; do
-    echo -n "(INV Ans)Do you want to install all(y/n): "
-    read install_A_check
-done
-echo -e ''
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+YELLO='\033[0;33m'
+RED='\033[0;31m'
 
-check_and_link(){
-    check='_'
-    if [[ $install_A_check = 'y' ]]; then
-        check='y'
-    else
-        echo -n "Do you want to link $1 (y/n): "
-        read check
-    fi
-    while [[ $check != @('y'|'n') ]]; do
-        echo -n "(INV Ans)Do you want to link $1 (y/n): "
-        read check
-    done
-    if [ $check = 'y' ] ; then
-        ln -f ./$1 ~/$1
-        echo -e "$1: \tlink $1 successful"
-    fi
+INFO="${GREEN}INFO: ${NC}"
+WARNING="${YELLO}WARNING: ${NC}"
+ERROR="${RED}ERROR: ${NC}"
+
+print_var(){
+    eval "var=$1"
+    eval echo "$1=\${${var}}"
 }
 
-link_map_to_home(){
-    sudo cp '/usr/share/X11/xkb/keycodes/evdev' './xkb/evdev.backup'
-    sudo cp './xkb/evdev' '/usr/share/X11/xkb/keycodes/'
-    sudo dpkg-reconfigure xkb-data
-    echo -e 'keymap: \tchange keyboards configuration successful'
+read_env () {
+    echo $(grep -v '^#' .env | grep -e "$1" | sed -e 's/.*=//')
 }
 
-change_key_map(){
-    unameOut="$(uname -s)"
-    case "${unameOut}" in
-        Linux*)     machine=Linux;;
-        Darwin*)    machine=Mac;;
-        CYGWIN*)    machine=Cygwin;;
-        MINGW*)     machine=MinGw;;
-        *)          machine="UNKNOWN:${unameOut}"
+#===== check env file =====
+if [ ! -f .env ]; then
+    echo ".env file not detect, please copy .env.example to .env and conf"
+    exit
+fi
+
+#===== check install file =====
+computer_type=$(read_env "computer_type")
+
+echo "${GREEN}This will install:"
+alias_file=$(read_env "alias_file")
+if [ $alias_file = 'y' ]; then
+    echo "copy .bash_aliases  to $HOME"
+    echo "copy .aliases       to $HOME"
+fi
+
+vim_config=$(read_env "vim_config")
+if [ $vim_config = 'y' ]; then
+    echo "copy .vimrc         to $HOME"
+fi
+
+ideavimrc=$(read_env "ideavimrc")
+if [ $ideavimrc = 'y' ]; then
+    echo "copy .ideavimrc     to $HOME"
+fi
+
+zsh=$(read_env "zsh")
+if [ $zsh = 'y' ]; then
+    echo "copy .zshrc         to $HOME"
+    echo "copy .p10k.zsh      to $HOME"
+    echo "install zsh and dependencies"
+fi
+
+keymap=$(read_env "keymap")
+if [ $keymap = 'y' ] || [ computer_type = 'm' ]; then
+    echo "change keymap R_ctrl and R_alt"
+fi
+echo "${NC}"
+
+#===== check correct =====
+while true; do
+    read -p "correct?(y/n)" yn
+    case $yn in
+        [Yy]* ) correct='y'; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
     esac
+done
+echo ""
 
-    check='_'
-    FILE='./xkb/evdev.backup'
-    if [[ ${machine} = "Linux" &&  ! -f "$FILE" ]]; then
-        if [[ $install_A_check = 'y' ]]; then
-            check='y'
-        else
-            echo -n "Do you want to change change the R_ctrl and R_alt buttons(y/n): "
-            read check
-        fi
-        if [ $check = 'y' ] ; then
-            link_map_to_home
-        fi
-        while [[ $check != @('y'|'n') ]]; do
-            echo -n "(INV Ans)Do you want to change change the R_ctrl and R_alt buttons(y/n): "
-            read check
-            if [ $check = 'y' ] ; then
-                link_map_to_home
-            fi
-        done
+#===== install =====
+
+if [ $alias_file = 'y' ]; then
+    if [ $computer_type = 'm' ]; then
+        ln -f .aliases $HOME
+        echo "${INFO}link .aliases successful"
     else
-        echo "keymap: already change keyboards configuration"
+        cp -f .aliases $HOME
+        echo "${INFO}cp .aliases successful"
     fi
-}
+fi
 
 
-### ================= change key map =================
-## chack OS and Ask user if they want to change the R_ctrl and R_alt buttons
-change_key_map
-check_and_link ".gitconfig"
-check_and_link ".vimrc"
-check_and_link ".bash_aliases"
+if [ $vim_config = 'y' ]; then
+    if [ $computer_type = 'm' ]; then
+        ln -f .vimrc $HOME
+        echo "${INFO}link .vimrc successful"
+    else
+        cp -f .vimrc_slave $HOME
+        echo "${INFO}cp .vimrc_slave successful"
+    fi
+fi
+
+if [ $ideavimrc = 'y' ]; then
+    if [ $computer_type = 'm' ]; then
+        ln -f .ideavimrc $HOME
+        echo "${INFO}link .ideavimrc successful"
+    else
+        cp -f .ideavimrc $HOME
+        echo "${INFO}cp .ideavimrc successful"
+    fi
+fi
+
+if [ $zsh = 'y' ]; then
+    if [ $computer_type = 'm' ]; then
+        ln -f .zshrc $HOME
+        echo "${INFO}link .zshrc successful"
+        ln -f .p10k.zsh $HOME
+        echo "${INFO}link .p10k.zsh successful"
+    else
+        cp -f .zshrc_slave $HOME
+        echo "${INFO}cp .zshrc_slave successful"
+        cp -f .p10k.zsh $HOME
+        echo "${INFO}cp .p10k.zsh successful"
+    fi
+
+fi
+
+if [ $keymap = 'y' ] || [ computer_type = 'm' ]; then
+    if [ ! -f ./xkb/evdev.backup ]; then
+        sudo cp '/usr/share/X11/xkb/keycodes/evdev' './xkb/evdev.backup'
+        sudo cp './xkb/evdev' '/usr/share/X11/xkb/keycodes/'
+        sudo dpkg-reconfigure xkb-data
+        echo "${INFO}keymap: \tchange keyboards configuration successful"
+    else
+        echo "${WARNING}./xkb/evdev.backup already exist"
+    fi
+fi
