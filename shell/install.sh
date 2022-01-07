5b00d17144385157eee917ec27018d27155d2196
shdennlin <shawndennislin@gmail.com>
2022-01-08 02:21:36 +0800

    [zsh] add -y to install curl

--- shell/install.sh
+++ shell/install.sh
@@ -23,7 +23,7 @@ if [ $zsh = 'y' ]; then
 
     # install ohmyzsh
     if [ ! -d "$HOME/.oh-my-zsh" ] ; then
-        sudo apt install curl
+        sudo apt install -y curl
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
     fi
     if [[ ! "zsh" == $(grep "^$USER" /etc/passwd | sed 's/.*://' | sed 's/.*\///') ]] ; then
