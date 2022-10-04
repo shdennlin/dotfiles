7fcbe294c4f3a82564c2b5f1500a29f4cc58fc2e
shdennlin <shawndennislin@gmail.com>
2022-10-04 16:35:01 +0800

    feat: update package

--- package/install.sh
+++ package/install.sh
@@ -2,12 +2,13 @@
 source ./functions.sh
 
 if [ $useful_package = 'y' ]; then
-    declare -a package=(
-        "fzf"       "fzf"
+	declare -a package=( # "<command>" "<package>"
+		"fzf"       "fzf"
         "exa"       "exa"
         "tmux"      "tmux"
         "batcat"    "bat"
 		"zoxide" 	"zoxide"
+		"ag"		"silversearcher-ag"
         )
     for ((i=0; i<${#package[@]}; i+=2)); do
         if ! command_exists ${package[$i]} ; then
