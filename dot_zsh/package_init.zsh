33cc55c0868f8e18f19c6d379e5367108546ad67
shdennlin <shawndennislin@gmail.com>
2025-02-22 16:26:25 +0800

    feat: Add navi to package list and update zsh integration

--- dot_zsh/package_init.zsh
+++ dot_zsh/package_init.zsh
@@ -6,44 +6,6 @@ if (( $+commands[zoxide] )); then
     eval "$(zoxide init zsh)"
 fi
 
-if (( $+commands[navi] )); then
-    # ref: https://github.com/denisidoro/navi/blob/master/shell/navi.plugin.zsh
-    _navi_call() {
-        local result="$(navi "$@" </dev/tty)"
-        printf "%s" "$result"
-    }
-
-    _navi_widget() {
-        local -r input="${LBUFFER}"
-        local -r last_command="$(echo "${input}" | navi fn widget::last_command)"
-        local replacement="$last_command"
-
-        if [ -z "$last_command" ]; then
-            replacement="$(_navi_call --print)"
-        elif [ "$LASTWIDGET" = "_navi_widget" ] && [ "$input" = "$previous_output" ]; then
-            replacement="$(_navi_call --print --query "$last_command")"
-        else
-            replacement="$(_navi_call --print --best-match --query "$last_command")"
-        fi
-
-        if [ -n "$replacement" ]; then
-            local -r find="${last_command}_NAVIEND"
-            previous_output="${input}_NAVIEND"
-            previous_output="${previous_output//$find/$replacement}"
-        else
-            previous_output="$input"
-        fi
-
-        zle kill-whole-line
-        LBUFFER="${previous_output}"
-        region_highlight=("P0 100 bold")
-        zle redisplay
-    }
-
-    zle -N _navi_widget
-    bindkey '^g' _navi_widget
-fi
-
 if (( $+commands[thefuck] || $+commands[fuck] )); then
     znap function _fuck fuck 'eval $(thefuck --alias)'
     compdef _fuck fuck
@@ -75,7 +37,16 @@ if (( $+commands[gh] )) && [ ! -f $FUNCTION_DIR/_gh ]; then
     znap compile $FUNCTION_DIR
 fi
 
+if (( $+commands[navi] )); then
+    eval "$(navi widget zsh)"
+fi
+
 if (( $+commands[chezmoi] )) && [ ! -f $FUNCTION_DIR/_chezmoi ]; then
     znap fpath _chezmoi "chezmoi completion zsh"
+    znap compile $FUNCTION_DIR
+fi
+
+if (( $+commands[rg] )) && [ ! -f $FUNCTION_DIR/_rg ]; then
+    znap fpath _rg "rg --generate=complete-zsh"
     znap compile $FUNCTION_DIR
 fi
